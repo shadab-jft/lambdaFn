dc58d231c007ef73e8b04ec0ffee0e76998c0974
Shadab <shadab.ahmed@jellyfishtechnologies.com>
2023-11-08 10:48:22 +0530

    changes

--- lambdaCreation.mjs
+++ lambdaCreation.mjs
@@ -2,8 +2,6 @@ import fetch from 'node-fetch'
 export const handler = async (event)=>{
     const apiUrl = `https://jsonplaceholder.typicode.com/todos/1`;
     const cookie = event.cookie;
-    const iterationTimes = event.iterationTimes;
-    let responseJson;
     async function makeApiCall() {
       try {
         const response = await fetch(apiUrl, {
@@ -12,20 +10,16 @@ export const handler = async (event)=>{
             // 'Cookie':cookie
           }
         });
-        responseJson = await response.json();
-        if (!response.ok) {
-          console.error(`API Error: Status ${response.status} Error: ${JSON.stringify(responseJson)}`);
-        } else {
-          console.log(`Message: ${JSON.stringify(responseJson)}`);
-        }
+
+        const responseJson = await response.json();
+        if (!response.ok) 
+          return {error: `API Error: Status ${response.status} Error: ${JSON.stringify(responseJson)}`};
+        else 
+          return {message: `Message: ${JSON.stringify(responseJson)}`};
       } catch (error) {
-        console.error('Error in makeApiCall:', error);
+        return {error: `Error in makeApiCall: ${error}`};
       }
     }
-  
-    for (let i = 0; i < iterationTimes; i++) {
-      await new Promise(resolve => setTimeout(resolve, 1000)); 
-      makeApiCall();
-    }
-    return {message:"Call Completed"}
+    return makeApiCall();
 }
+// console.log(await handler({}));
